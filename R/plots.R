83e1ff664fa1f2db2e17e1da98eefd4676d33926
Bart <bartsmeets86@gmail.com>
2015-06-16 16:03:41 +0200

    fix buildcheck fail

--- R/plots.R
+++ R/plots.R
@@ -11,7 +11,7 @@ licorice_center_plot<-function(data_transformed){
     geom_text(aes(x=as.numeric(question), y=-1.06, label=paste0(round(neg*100), "%")),data=data_transformed$percentages, size=3) +
     geom_text(aes(x=as.numeric(question), y=1.06, label=paste0(round(pos*100), "%")), data=data_transformed$percentages, size=3) +
     coord_flip() +
-    scale_y_continuous("", breaks = seq(-1,1,.5), limits = c(-1.1,1.1), labels=scales::percent) +
+    scale_y_continuous("", breaks = seq(-1,1,.5), limits = c(-1.1,1.1), labels=percent) +
     xlab("") + ylab("") +
     theme(legend.position="bottom") +
     scale_x_continuous(breaks=1:length(levels(data_transformed$main$question)), labels=levels(data_transformed$main$question))
@@ -33,7 +33,7 @@ licorice_fill_plot<-function(data_transformed){
     ggplot(data_transformed$main) +
     geom_bar(aes(x=question, fill=response, y=response_relative), position="fill", stat="identity") +
     coord_flip() +
-    scale_y_continuous(labels=scales::percent) +
+    scale_y_continuous(labels=percent) +
     xlab("") + ylab("") +
     theme(legend.position="bottom")
 
